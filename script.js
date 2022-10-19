c6a822722320de124a07ef35ea3341575fdf358b
ottovos <otto.vos@customers2you.com>
2022-10-19 17:20:07 +0200

    valuecommitted

--- script.js
+++ script.js
@@ -294,7 +294,7 @@ const init = function () {
   //score
   const arrLenghtLang1 = arrLanguage1.length;
   totalWords.textContent = arrLenghtLang1;
-  practWords.textContent = 5;
+  practWords.textContent = 0;
 };
 
 init();
